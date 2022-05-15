c1720d1a9480e699e97121fd0df198d81f51433e
sion <parksion9262@gmail.com>
2022-05-16 00:08:49 +0900

    Posts Front 구현 및 API 연동

--- src/main/java/com/sion/awsservice/service/posts/PostsService.java
+++ src/main/java/com/sion/awsservice/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.sion.awsservice.service.posts;
 
 import com.sion.awsservice.domain.posts.Posts;
 import com.sion.awsservice.domain.posts.PostsRepository;
+import com.sion.awsservice.web.dto.PostsListResponseDto;
 import com.sion.awsservice.web.dto.PostsResponseDto;
 import com.sion.awsservice.web.dto.PostsSaveRequestDto;
 import com.sion.awsservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,10 +32,25 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() ->  new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습ㄴ디ㅏ. id=" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
